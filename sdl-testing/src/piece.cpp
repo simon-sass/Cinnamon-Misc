30f58a67ce6ac4e11ad38fd64e2b5e7428591348
Simon Sass <simoncsass@gmail.com>
2023-04-03 16:57:29 -0500

    Improved readability
    Added a little demonstration

--- sdl-testing/src/piece.cpp
+++ sdl-testing/src/piece.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 using namespace std;
 
+/* Shapes look like:
+   [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+   ]
+This would make an L piece*/
 Piece::Piece(vector<vector<int>> shape) {
     this->shape = shape;
     this->size[0] = shape[0].size();
